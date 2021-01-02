f6c4d5d41ccf6bb08e6ae4d6742625647d39b63a
Anusha Kamat <anushasanyamkamat@gmail.com>
2021-01-02 22:28:21 +0100

    fix padding bottom on Order Summary page

--- src/views/FoodCart.vue
+++ src/views/FoodCart.vue
@@ -1,141 +1,146 @@
 <template>
-   <v-list three-line class="pb-18">
-       <v-row>
-            <v-col
-                cols="12"
-                sm="6"
-                offset-sm="3">
-                <v-card>
-                    <v-toolbar
-                        color="orange"
-                        dark>
-                            <v-icon
-                                large color="black">
-                                mdi-chevron-left
-                            </v-icon>
-                            <v-btn
-                                color="black"
-                                text
-                                rounded
-                                class="my-2"
-                                @click="backToMenu">
-                                Back To Menu
-                            </v-btn>      
-                        
-                    </v-toolbar>
-                </v-card>
+  <div>
+    <v-list three-line class="pb-18">
+      <v-row>
+        <v-col
+            cols="12"
+            sm="6"
+            offset-sm="3">
+          <v-card>
+            <v-toolbar
+                color="orange"
+                dark>
+              <v-icon
+                  large color="black">
+                mdi-chevron-left
+              </v-icon>
+              <v-btn
+                  color="black"
+                  text
+                  rounded
+                  class="my-2"
+                  @click="backToMenu">
+                Back To Menu
+              </v-btn>
+
+            </v-toolbar>
+          </v-card>
+        </v-col>
+      </v-row>
+      <template v-for="(item, $index) in foodCartItems">
+        <v-list-item :key="$index" v-if="item.quantity>0">
+          <v-list-item-avatar>
+            <v-img :src="item.image">
+            </v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-col>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
             </v-col>
-        </v-row>
-        <template v-for="(item, $index) in foodCartItems">
-            <v-list-item  :key="$index" v-if="item.quantity>0">
-                <v-list-item-avatar>
-                    <v-img :src="item.image">
-                    </v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                    <v-col>
-                    <v-list-item-title v-text="item.title"></v-list-item-title>
-                    </v-col>
-                    <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
-                    <v-col>
-                    <v-list-item-subtitle >Rs.{{item.price}}/- </v-list-item-subtitle>
-                    </v-col>
-                    <Counter :count="item.quantity" 
-                             @product-count="updateQuantity(item, $event)"/> 
-            
-                </v-list-item-content>
-            </v-list-item> 
-        </template>  
-        <v-footer 
-            padless
-            fixed
-            color="orange"
-            >
-            <hr>
-                <v-row>
-                    <v-col align-self="center" color="black">
-                         Total Price :-
-                    </v-col>
-                    <v-col justify="end" color = "black"> Rs.{{ totalPrice }}/-
-                    </v-col>
-                </v-row>
-                <v-row
-                    justify="end"
-                    no-gutters 
-                    >
-                    <v-btn
-                        color="black"
-                        text
-                        rounded
-                        class="my-2"
-                        @click="checkout">
-                        Proceed to checkout
-                        <v-icon
-                            large color="black">
-                            mdi-chevron-right
-                         </v-icon>
-                    </v-btn>
-                </v-row>
-        </v-footer>
+            <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
+            <v-col>
+              <v-list-item-subtitle>Rs.{{ item.price }}/-</v-list-item-subtitle>
+            </v-col>
+            <Counter :count="item.quantity"
+                     @product-count="updateQuantity(item, $event)"/>
+
+          </v-list-item-content>
+        </v-list-item>
+      </template>
     </v-list>
+    <v-footer
+        padless
+        fixed
+        color="orange"
+    >
+      <hr>
+      <v-row>
+        <v-col align-self="center" color="black">
+          Total Price :-
+        </v-col>
+        <v-col justify="end" color="black"> Rs.{{ totalPrice }}/-
+        </v-col>
+      </v-row>
+      <v-row
+          justify="end"
+          no-gutters
+      >
+        <v-btn
+            color="black"
+            text
+            rounded
+            class="my-2"
+            @click="checkout">
+          Proceed to checkout
+          <v-icon
+              large color="black">
+            mdi-chevron-right
+          </v-icon>
+        </v-btn>
+      </v-row>
+    </v-footer>
+
+  </div>
 </template>
 
 <script>
-    import Counter from '../components/Counter.vue'
-    export default {
-        name:'FoodCart',
-        components: {
-            Counter,
-        },
-        data: function(){
-            return{
-                foodCartItems:null,
-            }
-        },
-        created(){
-            this.foodCartItems= JSON.parse(localStorage.getItem('foodItems'));
+import Counter from '../components/Counter.vue'
 
-        },
-
-        computed: {
-        totalQuantity: function(){
-                return this.foodCartItems.reduce(function(total, item){
-                return total + item.quantity;
-                },0);
-            
-        },
-        totalPrice: function(){
-            return this.foodCartItems.reduce(function(total, item){
-                return total + (item.quantity * item.price);
-            },0);
-            },
-        },
-        methods: {
-            updateQuantity: function(item,quantity){
-            console.log(item,quantity);
-            item.quantity = quantity; 
-            localStorage.setItem('foodItems', JSON.stringify(this.foodCartItems));
-            
-            
-            },
-            backToMenu: function(){
-                this.$router.push('/');
-            },
-            checkout: function(){
-                this.$router.push('contact-info');
-                localStorage.setItem('totalPrice', JSON.stringify(this.totalPrice));
-                
-
-            },
-        },
+export default {
+  name: 'FoodCart',
+  components: {
+    Counter,
+  },
+  data: function() {
+    return {
+      foodCartItems: null,
     }
+  },
+  created() {
+    this.foodCartItems = JSON.parse(localStorage.getItem('foodItems'));
+
+  },
+
+  computed: {
+    totalQuantity: function() {
+      return this.foodCartItems.reduce(function(total, item) {
+        return total + item.quantity;
+      }, 0);
+
+    },
+    totalPrice: function() {
+      return this.foodCartItems.reduce(function(total, item) {
+        return total + (item.quantity * item.price);
+      }, 0);
+    },
+  },
+  methods: {
+    updateQuantity: function(item, quantity) {
+      console.log(item, quantity);
+      item.quantity = quantity;
+      localStorage.setItem('foodItems', JSON.stringify(this.foodCartItems));
+
+
+    },
+    backToMenu: function() {
+      this.$router.push('/');
+    },
+    checkout: function() {
+      this.$router.push('contact-info');
+      localStorage.setItem('totalPrice', JSON.stringify(this.totalPrice));
+
+
+    },
+  },
+}
 
 </script>
 
 <style>
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
+
 .pb-18 {
   padding-bottom: 72px;
 }
--- src/views/OrderSummary.vue
+++ src/views/OrderSummary.vue
@@ -1,107 +1,118 @@
 <template>
-    <v-list>
-        <v-row>
-            <v-col
-                cols="12"
-                sm="6"
-                offset-sm="3">
-                <v-card>
-                    <v-toolbar
-                        color="orange"
-                        dark>
-                            <v-icon
-                                large color="black">
-                                mdi-chevron-left
-                            </v-icon>
-                            <v-btn
-                                color="black"
-                                text
-                                rounded
-                                class="my-2"
-                                @click="contactForm">
-                                Back To Contact Form
-                            </v-btn>      
-                        
-                    </v-toolbar>
-                </v-card>
-            </v-col>
-        </v-row> 
-        <h2 style="margin-left:25px">Order Summary</h2>
-        <template v-for="(item, $index) in foodItems">
-            <v-list-item  :key="$index" v-if="item.quantity>0">
-                    <v-col>
-                    <v-list-item-title> Item Name: {{item.title}} || Qty: {{item.quantity}}
-                    </v-list-item-title>
-                    </v-col>
-            </v-list-item> 
-        </template>  
-        
-        <v-list-item>
-                <v-col>
-                <v-list-item-title>Total Price: Rs.{{price}}/- </v-list-item-title> <br>     
-                <v-list-item-title>Customer Name: {{name}} </v-list-item-title><br>
-                <v-list-item-title>Customer Address: {{address}} </v-list-item-title><br>
-                <v-list-item-title>Customer Phone No.: {{phone}}</v-list-item-title>
-                </v-col>
+  <div>
+    <v-list class="pb-18">
+      <v-row>
+        <v-col
+            cols="12"
+            sm="6"
+            offset-sm="3">
+          <v-card>
+            <v-toolbar
+                color="orange"
+                dark>
+              <v-icon
+                  large color="black">
+                mdi-chevron-left
+              </v-icon>
+              <v-btn
+                  color="black"
+                  text
+                  rounded
+                  class="my-2"
+                  @click="contactForm">
+                Back To Contact Form
+              </v-btn>
+
+            </v-toolbar>
+          </v-card>
+        </v-col>
+      </v-row>
+      <h2 style="margin-left:25px">Order Summary</h2>
+      <template v-for="(item, $index) in foodItems">
+        <v-list-item :key="$index" v-if="item.quantity>0">
+          <v-col>
+            <v-list-item-title> Item Name: {{ item.title }} || Qty: {{ item.quantity }}
+            </v-list-item-title>
+          </v-col>
         </v-list-item>
-        <v-footer 
-            fixed
-            >
-            <v-container>
-                <v-row justify="end">
-                    <v-btn 
-                    color="green"
-                    @click="sendToWhatsapp">
-                        Send To WhatsApp
-                    </v-btn>
-                </v-row>
-            </v-container>
-        </v-footer>
+      </template>
+
+      <v-list-item>
+        <v-col>
+          <v-list-item-title>Total Price: Rs.{{ price }}/-</v-list-item-title>
+          <br>
+          <v-list-item-title>Customer Name: {{ name }}</v-list-item-title>
+          <br>
+          <v-list-item-title>Customer Address: {{ address }}</v-list-item-title>
+          <br>
+          <v-list-item-title>Customer Phone No.: {{ phone }}</v-list-item-title>
+        </v-col>
+      </v-list-item>
     </v-list>
+    <v-footer
+        fixed
+    >
+      <v-container>
+        <v-row justify="end">
+          <v-btn
+              color="green"
+              @click="sendToWhatsapp">
+            Send To WhatsApp
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-footer>
+
+  </div>
 </template>
 <script>
 
 export default {
-    name: 'OrderSummary',
-    data: function(){
-        return {
-           name:'',
-           address:'',
-           phone: null,
-           price: null,
-           foodItems: null,
-        }
+  name: 'OrderSummary',
+  data: function() {
+    return {
+      name: '',
+      address: '',
+      phone: null,
+      price: null,
+      foodItems: null,
+    }
+  },
+  created() {
+    this.name = localStorage.getItem('name');
+    this.address = localStorage.getItem('address');
+    this.phone = localStorage.getItem('phone');
+    this.price = localStorage.getItem('totalPrice');
+    this.foodItems = JSON.parse(localStorage.getItem('foodItems'));
+  },
+  methods: {
+    contactForm: function() {
+      this.$router.push('/contact-info');
     },
-    created(){
-        this.name = localStorage.getItem('name');
-        this.address = localStorage.getItem('address');
-        this.phone = localStorage.getItem('phone');
-        this.price = localStorage.getItem('totalPrice');
-        this.foodItems = JSON.parse(localStorage.getItem('foodItems'));
-    },
-    methods: {
-        contactForm: function(){
-            this.$router.push('/contact-info');
-        },
-        sendToWhatsapp: function(){
-            let foodCartItems = this.foodItems.filter(item => item.quantity >0);
-            let result = foodCartItems.map(item => ({ name: item.title, quantity: item.quantity }));
-            let foodCartString = result.reduce((acc, item)=>{
-                return acc + "Item Name: " + item.name + " || " + "Qty: " + item.quantity + "%0a" 
-            }, '');
-           
-            let orderDetails = "Dear Kamat's Dreamland, I would like to place an order for :-" + "%0a" + "%0a" +
-                        foodCartString + 
-                        "Total Price: Rs." + this.price + "/-" + '%0a' + "%0a" +
-                        "Name: " + this.name + "%0a" + 
-                        "Address: " + this.address + '%0a' + 
-                        "Mobile No: " + this.phone + '%0a';      
-            console.log(orderDetails);
-            let url = "https://api.whatsapp.com/send?phone=+918087667239&text=" +orderDetails; 
-            location.replace(url);
+    sendToWhatsapp: function() {
+      let foodCartItems = this.foodItems.filter(item => item.quantity > 0);
+      let result = foodCartItems.map(item => ({ name: item.title, quantity: item.quantity }));
+      let foodCartString = result.reduce((acc, item) => {
+        return acc + "Item Name: " + item.name + " || " + "Qty: " + item.quantity + "%0a"
+      }, '');
 
-         
-        },
+      let orderDetails = "Dear Kamat's Dreamland, I would like to place an order for :-" + "%0a" + "%0a" +
+          foodCartString +
+          "Total Price: Rs." + this.price + "/-" + '%0a' + "%0a" +
+          "Name: " + this.name + "%0a" +
+          "Address: " + this.address + '%0a' +
+          "Mobile No: " + this.phone + '%0a';
+      console.log(orderDetails);
+      let url = "https://api.whatsapp.com/send?phone=+918087667239&text=" + orderDetails;
+      location.replace(url);
+
+
     },
+  },
 }
 </script>
+<style>
+.pb-18{
+  padding-bottom: 72px;
+}
+</style>

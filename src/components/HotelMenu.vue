b9c9f3e2ccf7dd77ffb8957d4f0b677c0bd25af4
Anusha Kamat <anushasanyamkamat@gmail.com>
2021-01-05 16:13:58 +0100

    design the menu bar and fix scroll issue. href jump to particular section of page

--- src/components/HotelMenu.vue
+++ src/components/HotelMenu.vue
@@ -1,82 +1,124 @@
 <template>
   <div>
-    <v-img class="banner-img" src="../assets/logo.png"></v-img>
-    <v-app-bar>
-      <v-tabs>
-        <ul>
-          <li>
-            <a href="">Favorite Dishes</a>
-          </li>
-          <li>
-            <a href="">Starters</a>
-          </li>
-          <li>
-            <a href="">Main Course</a>
-          </li>
-          <li>
-            <a href="">Deserts</a>
-          </li>
-          <li>
-            <a href="">Drinks</a>
-          </li>
-        </ul>
-
-      </v-tabs>
-    </v-app-bar>
-    <v-list three-line class="pb-18">
-      <template v-for="(item, $index) in foodItems">
-        <v-list-item :key="$index">
-          <v-list-item-avatar>
-            <v-img :src="item.image">
-            </v-img>
-          </v-list-item-avatar>
-          <v-list-item-content class="overflow-y-auto">
-            <v-col>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-col>
-            <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
-            <v-col>
-              <v-list-item-subtitle>Rs.{{ item.price }}/-</v-list-item-subtitle>
-            </v-col>
-            <Counter :count="item.quantity"
-                     @product-count="updateQuantity(item, $event)"/>
-
-          </v-list-item-content>
-        </v-list-item>
+    <div>
+      <template>
+        <h1 id="header-title">Kamat's DreamLand Family Bar and Restaurant</h1>
       </template>
-    </v-list>
-
-    <v-footer
-        padless
-        fixed
-        color="orange"
-    >
-      <hr>
-      <v-row>
-        <v-col align-self="center" color="black">
-          <strong> Total Price :- </strong>
-        </v-col>
-        <v-col justify="end" color="black"><strong>{{ totalPrice }}/-</strong>
-        </v-col>
-      </v-row>
-      <v-row
-          justify="end"
-          no-gutters
+    </div>
+    <div>
+      <template>
+        <v-app-bar
+            color="orange"
+            dark
+        >
+          <template>
+            <v-tabs>
+              <v-tab>
+                <v-btn icon>
+                  <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+              </v-tab>
+              <v-tab><a href="#favorite">Favorite Dishes</a></v-tab>
+              <v-tab><a href="#starters">Starters</a></v-tab>
+              <v-tab>Main Course</v-tab>
+              <v-tab>Desert</v-tab>
+              <v-tab>Drinks</v-tab>
+            </v-tabs>
+          </template>
+        </v-app-bar>
+      </template>
+    </div>
+    <hr>
+    <hr>
+    <div>
+      <v-sheet
+          id="scrolling-techniques-4"
+          class="overflow-y-auto"
+          max-height="600"
       >
-        <v-btn
-            color="black"
-            text
-            rounded
-            class="my-2"
-            @click="cartItems">
-          Proceed to Food Cart
-          <v-icon
-              large color="black">
-            mdi-chevron-right
-          </v-icon>
-        </v-btn>
-      </v-row>
-    </v-footer>
+        <v-container style="height: 1000px;">
+          <h2 id="favorite">Favorite Dishes</h2>
+          <v-list three-line class="pb-18">
+            <template v-for="(item, $index) in foodItems">
+              <v-list-item :key="$index">
+                <v-list-item-avatar>
+                  <v-img :src="item.image">
+                  </v-img>
+                </v-list-item-avatar>
+                <v-list-item-content class="overflow-y-auto">
+                  <v-col>
+                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                  </v-col>
+                  <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
+                  <v-col>
+                    <v-list-item-subtitle>Rs.{{ item.price }}/-</v-list-item-subtitle>
+                  </v-col>
+                  <Counter :count="item.quantity"
+                           @product-count="updateQuantity(item, $event)"/>
+
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+          <h2 id="starters">Starters</h2>
+          <v-list three-line class="pb-18">
+            <template v-for="(item, $index) in foodItems">
+              <v-list-item :key="$index">
+                <v-list-item-avatar>
+                  <v-img :src="item.image">
+                  </v-img>
+                </v-list-item-avatar>
+                <v-list-item-content class="overflow-y-auto">
+                  <v-col>
+                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                  </v-col>
+                  <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
+                  <v-col>
+                    <v-list-item-subtitle>Rs.{{ item.price }}/-</v-list-item-subtitle>
+                  </v-col>
+                  <Counter :count="item.quantity"
+                           @product-count="updateQuantity(item, $event)"/>
+
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-container>
+      </v-sheet>
+    </div>
+    <div>
+      <v-footer
+          padless
+          fixed
+          color="orange"
+      >
+        <hr>
+        <v-row>
+          <v-col align-self="center" color="black">
+            <strong> Total Price :- </strong>
+          </v-col>
+          <v-col justify="end" color="black"><strong>{{ totalPrice }}/-</strong>
+          </v-col>
+        </v-row>
+        <v-row
+            justify="end"
+            no-gutters
+        >
+          <v-btn
+              color="black"
+              text
+              rounded
+              class="my-2"
+              @click="cartItems">
+            Proceed to Food Cart
+            <v-icon
+                large color="black">
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-row>
+      </v-footer>
+    </div>
   </div>
 </template>
 
@@ -155,10 +197,9 @@ export default {
     },
 
     cartItems: function() {
-      if (this.totalPrice){
+      if (this.totalPrice) {
         this.$router.push('/food-cart');
-      }
-      else {
+      } else {
         alert("Please select food items");
       }
 
@@ -186,29 +227,27 @@ export default {
   padding-bottom: 72px;
 }
 
-.banner-img {
-  height: 20%;
-}
-
-ul {
-  list-style-type: none;
-  margin-left: 450px;
-  padding: 0;
-  overflow: hidden;
-  background-color: whitesmoke;
-
-}
-
-li {
-  float: left;
-
-}
-
-li a {
-  display: block;
-  color: black;
+#header-title {
   text-align: center;
-  padding: 16px;
-  text-decoration: none;
+  color: orangered;
+  font-family: "Comic Sans MS";
+  font-style: italic;
 }
+
+#favorite {
+  text-align: center;
+  color: black;
+  text-decoration: underline;
+  border: double;
+  border-color: orange;
+}
+
+#starters {
+  text-align: center;
+  color: black;
+  text-decoration: underline;
+  border: solid;
+  border-color: orange;
+}
+
 </style>
